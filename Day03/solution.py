62d04c6e075641f648c9b2e8f7b2c0cb086d4c29
Sreekaran <sreekaran.srinath@gmail.com>
2020-12-06 00:16:17 +0530

    Finished Day 3 FINALLY

--- Day03/solution.py
+++ Day03/solution.py
@@ -1,13 +1,27 @@
 inputFile = open("input.txt", 'r')
 lines = inputFile.readlines()
-treeCount = i = 0
 
+# Part 1
+
+treeCount = i = 0
 for line in lines:
     line = line.strip()
-
-for line in lines:
     if line[i % len(line)] == '#':
         treeCount += 1
     i += 3
-
 print(treeCount)
+
+# Part 2
+
+shiftList = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+prod = 1
+for shiftSet in shiftList:
+    treeCount = i = j = 0
+    while j < len(lines):
+        lines[j] = lines[j].strip()
+        if lines[j][i % len(lines[j])] == '#':
+            treeCount += 1
+        i += shiftSet[0]
+        j += shiftSet[1]
+    prod *= treeCount
+print(prod)
